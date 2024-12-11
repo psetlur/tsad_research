b1f19fafa157a043f2b69e87913075b72836acd6
Pranav Setlur <psetlur@gmail.com>
2024-12-11 16:40:03 +0530

    Reverted to one fixed platform anomaly, fixed config ratio

--- online_tsad/src/models/encoder.py
+++ online_tsad/src/models/encoder.py
@@ -124,22 +124,22 @@ class Encoder(pl.LightningModule):
             y_0_neg[i][0] = self.inject_platform(y_0_neg[i][0], s0_0_neg, s1_0_neg, s2_0_neg)
             meta_0_neg.append([s0_0_neg, s1_0_neg, s2_0_neg])
 
-            # Second platform anomaly
-            m1 = [fixed_level_1, fixed_start_1, fixed_length_1]
-            y_1[i][0] = self.inject_platform(y_1[i][0], *m1)
-            meta_1.append(m1)
+            # # Second platform anomaly
+            # m1 = [fixed_level_1, fixed_start_1, fixed_length_1]
+            # y_1[i][0] = self.inject_platform(y_1[i][0], *m1)
+            # meta_1.append(m1)
 
-            # Positive and negative samples for second anomaly
-            s0_1 = m1[0] + np.random.uniform(low=-TAU, high=TAU)
-            s1_1 = max(m1[1] + np.random.uniform(low=-TAU, high=TAU), 0)
-            s2_1 = max(m1[2] + np.random.uniform(low=-TAU, high=TAU), 0)
-            y_1_pos[i][0] = self.inject_platform(y_1_pos[i][0], s0_1, s1_1, s2_1)
+            # # Positive and negative samples for second anomaly
+            # s0_1 = m1[0] + np.random.uniform(low=-TAU, high=TAU)
+            # s1_1 = max(m1[1] + np.random.uniform(low=-TAU, high=TAU), 0)
+            # s2_1 = max(m1[2] + np.random.uniform(low=-TAU, high=TAU), 0)
+            # y_1_pos[i][0] = self.inject_platform(y_1_pos[i][0], s0_1, s1_1, s2_1)
 
-            s0_1_neg = m1[0] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[0] + np.random.uniform(low=TAU, high=POS_RANGE)
-            s1_1_neg = max(m1[1] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[1] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
-            s2_1_neg = max(m1[2] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[2] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
-            y_1_neg[i][0] = self.inject_platform(y_1_neg[i][0], s0_1_neg, s1_1_neg, s2_1_neg)
-            meta_1_neg.append([s0_1_neg, s1_1_neg, s2_1_neg])
+            # s0_1_neg = m1[0] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[0] + np.random.uniform(low=TAU, high=POS_RANGE)
+            # s1_1_neg = max(m1[1] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[1] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
+            # s2_1_neg = max(m1[2] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[2] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
+            # y_1_neg[i][0] = self.inject_platform(y_1_neg[i][0], s0_1_neg, s1_1_neg, s2_1_neg)
+            # meta_1_neg.append([s0_1_neg, s1_1_neg, s2_1_neg])
                 
             # ### Mean shift anomaly
             # m = [hist_sample(level_1_cdf, LEVEL_BINS), np.random.uniform(0, 0.5), hist_sample(length_1_cdf, LENGTH_BINS)]
@@ -159,29 +159,29 @@ class Encoder(pl.LightningModule):
             # meta_1_neg.append([s0, s1, s2])
 
         meta_0, meta_0_neg = np.array(meta_0), np.array(meta_0_neg)
-        meta_1, meta_1_neg = np.array(meta_1), np.array(meta_1_neg)
+        #meta_1, meta_1_neg = np.array(meta_1), np.array(meta_1_neg)
         #meta_1, meta_1_neg = np.array(meta_1), np.array(meta_1_neg)
 
+        outputs = self(torch.cat([x, y_0, y_0_pos, y_0_neg, x_pos], dim=0))
         #outputs = self(torch.cat([x, y_0, y_0_pos, y_0_neg, y_1, y_1_pos, y_1_neg, x_pos], dim=0))
-        outputs = self(torch.cat([x, y_0, y_0_pos, y_0_neg, y_1, y_1_pos, y_1_neg, x_pos], dim=0))
         #c_x, c_y_0, c_y_0_pos, c_y_0_neg, c_y_1, c_y_1_pos, c_y_1_neg, c_x_pos = torch.split(outputs, x.shape[0], dim=0)
-        c_x, c_y_0, c_y_0_pos, c_y_0_neg, c_y_1, c_y_1_pos, c_y_1_neg, c_x_pos = torch.split(outputs, x.shape[0], dim=0)
+        c_x, c_y_0, c_y_0_pos, c_y_0_neg, c_x_pos = torch.split(outputs, x.shape[0], dim=0)
 
         ### Anomalies should be close to the ones with the same type and similar hyperparameters, and far away from the ones with different types and normal.
-        loss_global_0 = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos, c_y_1, c_y_1_pos, c_y_1_neg], dim=0))
-        loss_global_1 = self.info_loss(c_y_1, c_y_1_pos, torch.cat([c_x, c_x_pos, c_y_0, c_y_0_pos, c_y_0_neg], dim=0))
-        loss_global = loss_global_0 + loss_global_1
-        #loss_global = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos], dim=0))
+        #loss_global_0 = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos, c_y_1, c_y_1_pos, c_y_1_neg], dim=0))
+        #loss_global_1 = self.info_loss(c_y_1, c_y_1_pos, torch.cat([c_x, c_x_pos, c_y_0, c_y_0_pos, c_y_0_neg], dim=0))
+        #loss_global = loss_global_0 + loss_global_1
+        loss_global = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos], dim=0))
 
         ### Anomalies with far away hyperparameters should be far away propotional to delta.
-        loss_local_0 = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
-        loss_local_1 = hard_negative_loss(c_y_1, c_y_1_pos, c_y_1_neg, meta_1, meta_1_neg)
-        loss_local = loss_local_0 + loss_local_1
-        #loss_local = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
+        #loss_local_0 = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
+        #loss_local_1 = hard_negative_loss(c_y_1, c_y_1_pos, c_y_1_neg, meta_1, meta_1_neg)
+        #loss_local = loss_local_0 + loss_local_1
+        loss_local = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
         
         ### Nomral should be close to each other, and far away from anomalies.
-        loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos, c_y_0_neg, c_y_1, c_y_1_pos, c_y_1_neg], dim=0))
-        #loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos, c_y_0_neg], dim=0))
+        #loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos, c_y_0_neg, c_y_1, c_y_1_pos, c_y_1_neg], dim=0))
+        loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos, c_y_0_neg], dim=0))
 
         loss = loss_global + loss_local + loss_normal
         self.log("train_loss", loss, prog_bar=True)
@@ -237,22 +237,22 @@ class Encoder(pl.LightningModule):
             y_0_neg[i][0] = self.inject_platform(y_0_neg[i][0], s0_0_neg, s1_0_neg, s2_0_neg)
             meta_0_neg.append([s0_0_neg, s1_0_neg, s2_0_neg])
 
-            # Second platform anomaly
-            m1 = [fixed_level_1, fixed_start_1, fixed_length_1]
-            y_1[i][0] = self.inject_platform(y_1[i][0], *m1)
-            meta_1.append(m1)
+            # # Second platform anomaly
+            # m1 = [fixed_level_1, fixed_start_1, fixed_length_1]
+            # y_1[i][0] = self.inject_platform(y_1[i][0], *m1)
+            # meta_1.append(m1)
 
-            # Positive and negative samples for second anomaly
-            s0_1 = m1[0] + np.random.uniform(low=-TAU, high=TAU)
-            s1_1 = max(m1[1] + np.random.uniform(low=-TAU, high=TAU), 0)
-            s2_1 = max(m1[2] + np.random.uniform(low=-TAU, high=TAU), 0)
-            y_1_pos[i][0] = self.inject_platform(y_1_pos[i][0], s0_1, s1_1, s2_1)
+            # # Positive and negative samples for second anomaly
+            # s0_1 = m1[0] + np.random.uniform(low=-TAU, high=TAU)
+            # s1_1 = max(m1[1] + np.random.uniform(low=-TAU, high=TAU), 0)
+            # s2_1 = max(m1[2] + np.random.uniform(low=-TAU, high=TAU), 0)
+            # y_1_pos[i][0] = self.inject_platform(y_1_pos[i][0], s0_1, s1_1, s2_1)
 
-            s0_1_neg = m1[0] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[0] + np.random.uniform(low=TAU, high=POS_RANGE)
-            s1_1_neg = max(m1[1] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[1] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
-            s2_1_neg = max(m1[2] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[2] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
-            y_1_neg[i][0] = self.inject_platform(y_1_neg[i][0], s0_1_neg, s1_1_neg, s2_1_neg)
-            meta_1_neg.append([s0_1_neg, s1_1_neg, s2_1_neg])
+            # s0_1_neg = m1[0] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[0] + np.random.uniform(low=TAU, high=POS_RANGE)
+            # s1_1_neg = max(m1[1] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[1] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
+            # s2_1_neg = max(m1[2] + np.random.uniform(low=NEG_RANGE, high=-TAU) if np.random.random() > 0.5 else m1[2] + np.random.uniform(low=TAU, high=POS_RANGE), 0)
+            # y_1_neg[i][0] = self.inject_platform(y_1_neg[i][0], s0_1_neg, s1_1_neg, s2_1_neg)
+            # meta_1_neg.append([s0_1_neg, s1_1_neg, s2_1_neg])
                 
             # ### Mean shift anomaly
             # m = [hist_sample(level_1_cdf, LEVEL_BINS), np.random.uniform(0, 0.5), hist_sample(length_1_cdf, LENGTH_BINS)]
@@ -272,26 +272,26 @@ class Encoder(pl.LightningModule):
             # meta_1_neg.append([s0, s1, s2])
 
         meta_0, meta_0_neg = np.array(meta_0), np.array(meta_0_neg)
-        meta_1, meta_1_neg = np.array(meta_1), np.array(meta_1_neg)
+        #meta_1, meta_1_neg = np.array(meta_1), np.array(meta_1_neg)
        #meta_1, meta_1_neg = np.array(meta_1), np.array(meta_1_neg)
 
-        outputs = self(torch.cat([x, y_0, y_0_pos, y_0_neg, y_1, y_1_pos, y_1_neg, x_pos], dim=0))
-        c_x, c_y_0, c_y_0_pos, c_y_0_neg, c_y_1, c_y_1_pos, c_y_1_neg, c_x_pos = torch.split(outputs, x.shape[0], dim=0)
-        # outputs = self(torch.cat([x, y_0, y_0_pos, y_0_neg, x_pos], dim=0))
-        # c_x, c_y_0, c_y_0_pos, c_y_0_neg, c_x_pos = torch.split(outputs, x.shape[0], dim=0)
+        #outputs = self(torch.cat([x, y_0, y_0_pos, y_0_neg, y_1, y_1_pos, y_1_neg, x_pos], dim=0))
+        #c_x, c_y_0, c_y_0_pos, c_y_0_neg, c_y_1, c_y_1_pos, c_y_1_neg, c_x_pos = torch.split(outputs, x.shape[0], dim=0)
+        outputs = self(torch.cat([x, y_0, y_0_pos, y_0_neg, x_pos], dim=0))
+        c_x, c_y_0, c_y_0_pos, c_y_0_neg, c_x_pos = torch.split(outputs, x.shape[0], dim=0)
      
-        loss_global_0 = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos, c_y_1, c_y_1_pos], dim=0))
-        loss_global_1 = self.info_loss(c_y_1, c_y_1_pos, torch.cat([c_x, c_x_pos, c_y_0, c_y_0_pos], dim=0))
-        loss_global = loss_global_0 + loss_global_1
-        #loss_global = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos], dim=0))
+        #loss_global_0 = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos, c_y_1, c_y_1_pos], dim=0))
+        #loss_global_1 = self.info_loss(c_y_1, c_y_1_pos, torch.cat([c_x, c_x_pos, c_y_0, c_y_0_pos], dim=0))
+        #loss_global = loss_global_0 + loss_global_1
+        loss_global = self.info_loss(c_y_0, c_y_0_pos, torch.cat([c_x, c_x_pos], dim=0))
 
-        loss_local_0 = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
-        loss_local_1 = hard_negative_loss(c_y_1, c_y_1_pos, c_y_1_neg, meta_1, meta_1_neg)
-        loss_local = loss_local_0 + loss_local_1
-        #loss_local = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
+        #loss_local_0 = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
+        #loss_local_1 = hard_negative_loss(c_y_1, c_y_1_pos, c_y_1_neg, meta_1, meta_1_neg)
+        #loss_local = loss_local_0 + loss_local_1
+        loss_local = hard_negative_loss(c_y_0, c_y_0_pos, c_y_0_neg, meta_0, meta_0_neg)
         
-        loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos, c_y_1, c_y_1_pos], dim=0))
-        #loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos], dim=0))
+        #loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos, c_y_1, c_y_1_pos], dim=0))
+        loss_normal = self.info_loss(c_x, c_x_pos, torch.cat([c_y_0, c_y_0_pos], dim=0))
 
         loss = loss_global + loss_local + loss_normal
         self.log("loss_global", loss_global, prog_bar=True)
