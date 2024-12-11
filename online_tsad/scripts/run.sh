451dab9eb7536f8107875313c807cff661767e0c
Pranav Setlur <psetlur@gmail.com>
2024-12-11 18:09:00 +0530

    Modified script to only include one anom

--- online_tsad/scripts/run.sh
+++ online_tsad/scripts/run.sh
@@ -57,46 +57,25 @@
 #     # --wandb
 
 #!/bin/bash
-# Used for training TSA-AP model with a single fixed platform anomaly
+# Used for training TSA-AP model
 set -e
 
-### Anomaly Type: platform
-anomaly_type_1="platform"
+### Anomaly Types: platform, trend, extremum, mean, amplitude, frequency, shift
+### Only platform and mean support histogram distribution now
+anomaly_type="platform"
 anomaly_num="1"
 gpu=0
 time_id=`date +"%Y%m%d_%H%M%S"`
 clean_df_path="data/processed/cmu_motion.pkl"
 
-# Define fixed parameters for platform anomaly
-best_point="{
-    'ratio_anomaly': 0.1,  # Total ratio of anomalies (e.g., 10% of the data)
-    'fixed_level': 0.5,    # Fixed level for the platform anomaly
-    'fixed_length': 0.3,   # Fixed length for the platform anomaly
-    'fixed_start': 0.2     # Fixed start position for the platform anomaly
-}"
-
-train_data_name_1="mot_${anomaly_type_1}_${anomaly_num}_scaled"
+train_data_name="mot_${anomaly_type}_${anomaly_num}_scaled"
 python src/anom_generator/anomalies.py \
     --data_path $clean_df_path \
-    --anom_type $anomaly_type_1 \
+    --anom_type $anomaly_type \
     --truncated_length 512 \
     --aug_num 1 \
-    --name $train_data_name_1 \
-    --seed 0 \
-    --fixed_parameters "$best_point"
-
-### Merge data of the single anomaly
-anomaly_type="single_fixed"
-train_data_name="mot_single_${anomaly_num}_hist"
-train_ratio=0.8
-val_ratio=0.1
-anomaly_ratio=0.1
-python src/utils/merge_data.py \
-    --data_path_1 $train_data_name_1 \
-    --store_path $train_data_name \
-    --train_ratio $train_ratio \
-    --val_ratio $val_ratio \
-    --anomaly_ratio $anomaly_ratio
+    --name $train_data_name \
+    --seed 0
 
 ### Run algorithm
 base_name="a=${anomaly_num}_${anomaly_type}"
@@ -108,4 +87,5 @@ CUDA_VISIBLE_DEVICES=$gpu python src/main.py \
     --data_path "data/${train_data_name}" \
     --ckpt_name "$base_name" \
     --ckpt_monitor "$monitor" \
-    --config_path "$config_path"
+    --config_path "$config_path" \
+    # --wandb
