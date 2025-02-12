819de9145781c7efd910e65ba999e28d73873415
Pranav Setlur <psetlur@gmail.com>
2025-02-11 20:32:55 -0500

    Removed negatives from global loss computation

--- online_tsad/src/models/encoder.py
+++ online_tsad/src/models/encoder.py
@@ -189,14 +189,14 @@ class Encoder(pl.LightningModule):
             weight_global = 1.0
             weight_local = 1.0
 
-        loss_global = sum(self.info_loss(c_x, c_y_p, torch.cat([c_x] + list(c_y_neg), dim=0)) for c_y_p in c_y_pos) / len(c_y_pos)
+        loss_global = sum(self.info_loss(c_x, c_y_p, torch.cat([c_x] + [c_x], dim=0)) for c_y_p in c_y_pos) / len(c_y_pos)
         
         ### Anomalies with far away hyperparameters should be far away propotional to delta.
         loss_local = sum(hard_negative_loss(c_x, c_y_p, torch.stack(c_y_neg), meta_pos[i], meta_neg[i]) 
-                     for i, c_y_p in enumerate(c_y_pos)) / len(c_y_pos)
+                         for i, c_y_p in enumerate(c_y_pos)) / len(c_y_pos)
 
         ### Nomral should be close to each other, and far away from anomalies.
-        loss_normal = self.info_loss(c_x, c_x, torch.cat([torch.cat(c_y_pos, dim=0), torch.cat(c_y_neg, dim=0)], dim=0)) 
+        loss_normal = self.info_loss(c_x, torch.cat(c_y_pos, dim=0), torch.cat([c_x] + c_y_pos, dim=0)) 
 
         loss = weight_global * loss_global + weight_local * loss_local + weight_normal * loss_normal
         self.log("train_loss", loss, prog_bar=True)
@@ -258,13 +258,12 @@ class Encoder(pl.LightningModule):
         c_y_neg = split_outputs[num_positives + 1:num_positives + 1 + num_negatives]
         c_x_pos = split_outputs[-1]
 
-        loss_global = sum(self.info_loss(c_x, c_y_p, torch.cat([c_x] + list(c_y_neg), dim=0)) for c_y_p in c_y_pos) / len(c_y_pos)
+        loss_global = sum(self.info_loss(c_x, c_y_p, torch.cat([c_x] + [c_x], dim=0)) for c_y_p in c_y_pos) / len(c_y_pos)
 
-        loss_local = sum(hard_negative_loss(c_x, c_y_p, torch.stack(c_y_neg), meta_pos[i], meta_neg[i])
-                     for i, c_y_p in enumerate(c_y_pos)) / len(c_y_pos)
+        loss_local = sum(hard_negative_loss(c_x, c_y_p, torch.stack(c_y_neg), meta_pos[i], meta_neg[i]) 
+                         for i, c_y_p in enumerate(c_y_pos)) / len(c_y_pos)
 
-        loss_normal = self.info_loss(c_x, c_x_pos,
-                             torch.cat([torch.cat(c_y_pos, dim=0), torch.cat(c_y_neg, dim=0)], dim=0))
+        loss_normal = self.info_loss(c_x, torch.cat(c_y_pos, dim=0), torch.cat([c_x] + c_y_pos, dim=0)) 
         
         loss = loss_global + loss_local + loss_normal
         self.log("loss_global", loss_global, prog_bar=True)
